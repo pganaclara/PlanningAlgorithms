810d2e5c9bd61dd077823981313ad275826e189e
Ana Gonçalves <49643663+pganaclara@users.noreply.github.com>
2024-03-12 01:12:40 -0300

    division of phases and basic concatenation

--- PlanningAlgorithms/Algorithms.SCO.cs
+++ PlanningAlgorithms/Algorithms.SCO.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using PlanningDES;
 using UltraDES;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 using TimeContext = System.ValueTuple<System.Collections.Immutable.ImmutableList<UltraDES.AbstractEvent>, PlanningDES.Scheduler, PlanningDES.Restriction, float>;
 
 namespace PlanningAlgorithms
@@ -19,8 +20,8 @@ namespace PlanningAlgorithms
 
             var target = problem.TargetState;
             var schOrig = problem.InitialScheduler;
-            var resOrig = problem.InitialRestrition(products);
-            var depth = products * problem.Depth;
+            var resOrig = problem.InitialRestrition(1); //1 product
+            var depth = 1 * problem.Depth; // 1 product
             var uncontrollables = problem.Events.Where(e => !e.IsControllable).ToSet();
             var transitions = problem.Transitions;
 
@@ -29,10 +30,138 @@ namespace PlanningAlgorithms
                 {initial, new List<TimeContext>{(ImmutableList<AbstractEvent>.Empty, schOrig, resOrig, 0f)}}
             };
 
+            for (var it = 0; it < depth; it++)
+            {
+                var newFrontier = new Dictionary<AbstractState, List<TimeContext>>(frontier.Count);
+
+                void Loop(KeyValuePair<AbstractState, List<TimeContext>> kvp)
+                {
+                    var q1 = kvp.Key;
+                    foreach (var (sequence1, sch1, res1, time1) in kvp.Value)
+                    {
+                        var events = res1.Enabled;
+                        events.UnionWith(uncontrollables);
+                        events.IntersectWith(sch1.Enabled);
+                        events.IntersectWith(transitions[q1].Keys);
+
+                        if (controllableFirst && events.Any(e => e.IsControllable)) events.ExceptWith(uncontrollables);
+
+                        foreach (var e in events)
+                        {
+                            var q2 = transitions[q1][e];
+
+                            var sequence2 = sequence1.Add(e);
+                            var time2 = time1 + sch1[e];
+                            var res2 = e.IsControllable ? res1.Update(e) : res1;
+                            var sch2 = sch1.Update(e);
+
+                            var context2 = (sequence2, sch2, res2, time2);
+
+                            var contextLst = new List<TimeContext>();
+
+                            lock (newFrontier)
+                            {
+                                if (!newFrontier.ContainsKey(q2)) newFrontier.Add(q2, contextLst);
+                                else contextLst = newFrontier[q2];
+                            }
+
+                            lock (contextLst)
+                            {
+                                var candidate = contextLst.Where(c =>
+                                {
+                                    var (_, sch, res, _) = c;
+                                    return problem.Events.All(ev => sch2[ev] == sch[ev]); //&& problem.Events.All(ev => res2[ev] == res[ev]);
+                                }).ToList();
+
+                                if (candidate.Any() && candidate.Single().Item4 <= time2) continue;
+                                if (candidate.Any() && candidate.Single().Item4 > time2) contextLst.Remove(candidate.Single());
+                                contextLst.Add(context2);
+                            }
+                        }
+                    }
+                }
+
+                if (frontier.Count > limiar) Partitioner.Create(frontier, EnumerablePartitionerOptions.NoBuffering).AsParallel().ForAll(Loop);
+                else foreach (var kvp in frontier) Loop(kvp);
+
+                frontier = newFrontier;
+
+                Debug.WriteLine($"Frontier: {frontier.Count} elements");
+            }
+
             if (!frontier.ContainsKey(target))
                 throw new Exception($"The algorithm could not reach the targer ({target})");
 
-            return frontier[target].OrderBy(c => c.Item4).First().Item1.ToArray();
+            AbstractEvent[] group0 = frontier[target].OrderBy(c => c.Item4).First().Item1.ToArray(); //concat group0 and group1 
+            AbstractEvent[] group1 = frontier[target].OrderBy(c => c.Item4).First().Item1.ToArray(); //concat group0 and group1 
+
+            if (products == 1)
+            {
+                return group1;
+            } else
+            {
+                AbstractEvent[] concatenatedArray = ConcatenateArrays(group0, group1, 2);
+                 int initialSize = concatenatedArray.Length / 4;
+                 int productionSize = 2 * initialSize;
+                
+
+                   AbstractEvent[] initialPhase = concatenatedArray.Take(initialSize).ToArray(); //0s
+                   AbstractEvent[] productionPhase = concatenatedArray.Skip(initialSize).Take(productionSize).ToArray(); //first half 0s, scnd 1s
+                   AbstractEvent[] finalPhase = concatenatedArray.Skip(initialSize + productionSize).ToArray(); //1s
+
+                   AbstractEvent[] finalArray = GenerateFinalArray(initialPhase, productionPhase, finalPhase, products);
+                return finalArray;
+
+            }
+        }
+
+        static AbstractEvent[] GenerateFinalArray(AbstractEvent[] initialPhase, AbstractEvent[] productionPhase, AbstractEvent[] finalPhase, int numberOfProducts)
+        {
+            int productionRepeatCount = numberOfProducts / 2;
+            int midpoint = productionPhase.Length / 2;
+
+            AbstractEvent[] firstHalf = productionPhase.Take(midpoint).ToArray(); //0s
+            AbstractEvent[] secondHalf = productionPhase.Skip(midpoint).ToArray(); //1s
+            AbstractEvent[] reversedProductionPhase = finalPhase.Concat(initialPhase).ToArray();
+
+            var finalArray = new List<AbstractEvent>();
+
+            finalArray.AddRange(initialPhase);
+
+            for (int i = 0; i < productionRepeatCount; i++)
+            {
+                if(numberOfProducts != 2)
+                {
+                    finalArray.AddRange(productionPhase);
+                    finalArray.AddRange(reversedProductionPhase);
+                }
+            }
+
+            if (numberOfProducts % 2 == 0)
+            {
+                finalArray.AddRange(productionPhase);
+                finalArray.AddRange(finalPhase);
+            } else
+            {
+                finalArray.AddRange(reversedProductionPhase);
+                finalArray.AddRange(firstHalf);
+            }
+
+            return finalArray.ToArray();
+        }
+
+
+
+        static AbstractEvent[] ConcatenateArrays(AbstractEvent[] array0, AbstractEvent[] array1, int products)
+        {
+            int length0 = array0.Length;
+            int length1 = array1.Length;
+
+            AbstractEvent[] concatenatedArray = Enumerable.Range(0, products)
+                                          .SelectMany(i => i % 2 == 0 ? array0 : array1)
+                                          .ToArray();
+
+            return concatenatedArray;
         }
     }
 }
--- PlanningAlgorithms/Program.cs
+++ PlanningAlgorithms/Program.cs
@@ -4,6 +4,7 @@ using PlanningDES.Problems;
 using UltraDES;
 using ConsoleTables;
 using static PlanningAlgorithms.Algorithms;
+using System.Linq;
 
 namespace PlanningAlgorithms
 {
@@ -14,34 +15,42 @@ namespace PlanningAlgorithms
             System.Threading.Thread.CurrentThread.Priority = System.Threading.ThreadPriority.AboveNormal;
             System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
+            //var problem = new SmallFactory();
             var problem = new FlexibleManufacturingSystem();
-            var algorithms = new (string, Func<int, AbstractEvent[]>)[3];
-            algorithms[0] = ("Parallelism Maximization with Time Restrictions", (p) => PMT(problem, p, controllableFirst: true));
-            algorithms[1] = ("Heuristic Makespan Minimization", (p) => HMM(problem, p, controllableFirst: true));
-            algorithms[2] = ("Supervisory Control and Optimization", (p) => SCO(problem, p, controllableFirst: true));
-            //algorithms[2] = ("Parallelism Maximization (Logic)", (p) => PML(problem, p, controllableFirst: false));
+            var algorithms = new (string, Func<int, AbstractEvent[]>)[2];
+            //algorithms[0] = ("Parallelism Maximization with Time Restrictions", (p) => PMT(problem, p, controllableFirst: true));
+            algorithms[0] = ("Heuristic Makespan Minimization", (p) => HMM(problem, p, controllableFirst: true));
+            algorithms[1] = ("Supervisory Control and Optimization", (p) => SCO(problem, p, controllableFirst: true));
+            //algorithms[3] = ("Parallelism Maximization (Logic)", (p) => PML(problem, p, controllableFirst: false));
 
             foreach (var (name, algorithm) in algorithms)
             {
                 var table = new ConsoleTable("Batch", "Time", "Makespan", "Parallelism");
-                foreach (var products in new[] { 1, 10, 100, 1000 })
+                foreach (var products in new[] { 1, 5, 10, 15, 50, 100 })
                 {
                     Func<AbstractEvent[]> test = () => algorithm(products);
 
                     var (time, sequence) = test.Timming();
+                    /*nsole.Write($"\nSequence {products} products. Size {sequence.Length}: \n");
+                    foreach (var e in sequence)
+                    {
+                        Console.Write($"{e} | ");
+                    }
+                    Console.Write("\n");*/
                     var makespan = problem.TimeEvaluation(sequence);
                     var parallelism = problem.MetricEvaluation(sequence, (t) => t.destination.ActiveTasks());
 
-                    table.AddRow(products, time, makespan, parallelism);
+                    
+
+                    table.AddRow(products, time, makespan, parallelism); 
                 }
 
-                Console.WriteLine($"{name}:");
+                Console.WriteLine($"\n{name}:");
                 table.Write(Format.Alternative);
                 Console.WriteLine();
+
             }
 
-
-            
         }
     }
 }
